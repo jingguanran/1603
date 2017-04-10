5b6a2e3342c0a07f7b0faaa7fe62d3fe6dca8cff
jingguanran <jingguanran@126.com>
2017-04-10 08:42:38 +0800

    啊是十分

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ffApp
+ * @description
+ * # ffApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ffApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ffApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ffApp
+ */
+angular.module('ffApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
